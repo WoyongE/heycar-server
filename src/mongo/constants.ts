39bdd4edaa3a74d2fac786177fc26d3656102c6c
Woyong Egbara <woyongoe@gmail>
2022-06-26 01:40:03 +0200

    Adding test env file, clean up.

--- src/mongo/constants.ts
+++ src/mongo/constants.ts
@@ -1,7 +1,6 @@
-import { isTest } from '../constants';
 import { mongoClient } from './mongo';
 
-const databaseName = isTest ? 'hey_car_test' : process.env.DATABASE_NAME;
+const databaseName = process.env.DATABASE_NAME;
 const database = mongoClient.db(databaseName);
 const usersCollectionName = 'users';
 const productsCollectionName = 'products';
